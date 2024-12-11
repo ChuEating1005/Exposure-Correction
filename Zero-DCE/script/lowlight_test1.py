c7fc0939e9826f85ae5c265b8dc0d36d25579687
ChuEating1005 <itingchu1005@gmail.com>
2024-12-12 01:27:29 +0800

    modify

--- Zero-DCE/script/lowlight_test1.py
+++ Zero-DCE/script/lowlight_test1.py
@@ -31,7 +31,7 @@ def lowlight(image_path, weight_TV=200, weight_spa=1, weight_col=5, weight_exp=1
 	data_lowlight = data_lowlight.cuda().unsqueeze(0)
 
 	DCE_net = model.enhance_net_nopool().cuda()
-	DCE_net.load_state_dict(torch.load(f'snapshots/exp{weight_exp}_col{weight_col}_Epoch190.pth'))
+	DCE_net.load_state_dict(torch.load(f'snapshots/Epoch199.pth'))
 	start = time.time()
 	_,enhanced_image,_ = DCE_net(data_lowlight)
 
@@ -54,17 +54,10 @@ if __name__ == '__main__':
 		# filePath = 'data/test_data/'
 	
 		# file_list = os.listdir(filePath)
-		file_list = ['DICM/29.jpg', 'DICM/32.jpg', 'DICM/37.jpg', 'DICM/44.jpg', 'DICM/47_2.jpg', 'DICM/70.jpg']
-		file_list = ['data/test_data/' + i for i in file_list]
+		file = 'data/test_data/DICM/70.jpg'
 		for weight_exp in range(10, 35, 5):
 			for weight_col in range(5, 25, 5):
-				for file_name in file_list:
-					# test_list = glob.glob(filePath+file_name+"/*") 
-
-					# for image in test_list:
-						# image = image
-						# print(image)
-						lowlight(file_name, weight_col=weight_col, weight_exp=weight_exp)
+				lowlight(file, weight_col=weight_col, weight_exp=weight_exp)
 
 		
 
